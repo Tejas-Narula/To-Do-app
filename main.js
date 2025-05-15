0406c092ef90649aacdcf92263eec10eddc90436
Tejas Narula <tejasnarula@gmail.com>
2025-05-16 01:50:29 +0530

    Added Delete task functionality

--- main.js
+++ main.js
@@ -7,13 +7,29 @@ renderTodoList()
 function renderTodoList(){
     let todoTasksHtml = ''
     tasks.forEach(task => {
-        html = `<div class="task">${task} <input type="checkbox"></div>`
+        html = `<div class="task">${task} <input type="checkbox" class="js-checkBox"></div>`
         todoTasksHtml += html
     });
 
     document.querySelector(".js-Tasks").innerHTML = todoTasksHtml
 
+    //checkbox
+    document.querySelectorAll(".js-checkBox").forEach((checkbox, index) =>{
+        checkbox.addEventListener("change", () => {
+        if (checkbox.checked){
+            tasks.splice(index,1)
+            renderTodoList()
+        }
+    })
+    })
+
+
 }
+
+
+
+
+
 
 // Add Task Button
 document.querySelector(".js-taskInputBtn").addEventListener("click", () =>{
