1014d048c976db4cf950cdd597186425c4d26978
Tejas Narula <tejasnarula@gmail.com>
2025-05-16 01:36:02 +0530

    basic css and js (v0.1)

---
+++ main.js
@@ -0,0 +1,33 @@
+// todo list
+const tasks = ["Exercise", "Walk"]
+renderTodoList()
+
+
+// render todo list by creating the html and putting it on the page
+function renderTodoList(){
+    let todoTasksHtml = ''
+    tasks.forEach(task => {
+        html = `<div class="task">${task} <input type="checkbox"></div>`
+        todoTasksHtml += html
+    });
+
+    document.querySelector(".js-Tasks").innerHTML = todoTasksHtml
+
+}
+
+// Add Task Button
+document.querySelector(".js-taskInputBtn").addEventListener("click", () =>{
+    addTask();
+})
+
+function addTask(){
+    const taskInput = document.querySelector('.js-taskInput');
+
+    const task = taskInput.value;
+
+    tasks.push(task);  
+
+    taskInput.value = '';
+
+    renderTodoList()
+}
